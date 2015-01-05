c4b7ed1ff157e121a63e222840c7586189fafda2
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-05 14:18:08 -0800

    Cleaned out testing code

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 server = http.createServer(function (request, response) {
-	//// For Testing Purposes ////
-	// console.log('\n\n\nRequest Output\n', request);
-	// console.log('\n\n\nResponse Output\n', response);
 	response.writeHead(200, {'Content-type': 'text/html'});
 	console.log('Request', request.url);
 	if (request.url === '/') {
