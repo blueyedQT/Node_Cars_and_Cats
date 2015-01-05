96b841cdd56121803d6bf4dbf63bed5cd95ffea5
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-05 13:36:25 -0800

    Cats displaying

--- app.js
+++ app.js
@@ -7,33 +7,63 @@ server = http.createServer(function (request, response) {
 	// console.log('\n\n\nResponse Output\n', response);
 	response.writeHead(200, {'Content-type': 'text/html'});
 	console.log('Request', request.url);
-	if(request.url === '/') {
+	if (request.url === '/') {
 		fs.readFile('views/index.html', 'utf-8', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
-	} else if(request.url === '/cars') {
-		fs.readFile('views/cars.html', 'utf-8', function (errors, contents) {
+	} else if (request.url === '/cars') {
+		fs.readFile('views/cars.html', 'utf-8', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
-	} else if(request.url === '/images/fordGT.jpg') {
-		fs.readFile('images/fordGT.jpg', function(errors, contents){
+	} else if (request.url === '/images/fordGT.jpg') {
+		fs.readFile('images/fordGT.jpg', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
-	} else if(request.url === '/images/saleen.jpg') {
-		fs.readFile('images/saleen.jpg', function(errors, contents){
+	} else if (request.url === '/images/saleen.jpg') {
+		fs.readFile('images/saleen.jpg', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
-	} else if(request.url === '/images/viper.jpg') {
-		fs.readFile('images/viper.jpg', function(errors, contents){
+	} else if (request.url === '/images/viper.jpg') {
+		fs.readFile('images/viper.jpg', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
-	} else if(request.url === '/images/mini.jpg') {
-		fs.readFile('images/mini.jpg', function(errors, contents){
+	} else if (request.url === '/images/mini.jpg') {
+		fs.readFile('images/mini.jpg', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/cats') {
+		fs.readFile('views/cats.html', 'utf-8', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/images/cats1.jpg') {
+		fs.readFile('images/cats1.jpg', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/images/cats2.jpg') {
+		fs.readFile('images/cats2.jpg', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/images/cats3.jpg') {
+		fs.readFile('images/cats3.jpg', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/images/cats4.jpg') {
+		fs.readFile('images/cats4.jpg', function(errors, contents) {
+			response.write(contents);
+			response.end();
+		});
+	} else if (request.url === '/styles/style.css') {
+		fs.readFile('styles/style.css', function(errors, contents) {
 			response.write(contents);
 			response.end();
 		});
